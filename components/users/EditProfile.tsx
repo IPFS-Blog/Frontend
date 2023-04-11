cbe60bf7fa0fee0a0d1b73a250fc54b96af239b3
jim0625 <a0931052056@gmail.com>
2023-04-11 14:23:44 +0800

    fix: 加入email修改

--- components/users/EditProfile.tsx
+++ components/users/EditProfile.tsx
@@ -26,6 +26,7 @@ export default function Editprofile() {
   return (
     <>
       <div>
+        {/*  dialog部分皆為彈窗*/}
         <Button variant="outlined" onClick={handleClickOpen}>
           Open responsive dialog
         </Button>
@@ -38,30 +39,34 @@ export default function Editprofile() {
         >
           <DialogTitle id="responsive-dialog-title">{"EditProfile"}</DialogTitle>
           <DialogContent>
+            {/* 彈窗後整個畫面設計 */}
             <div className={styles.all}>
               <div className="ml-8">
                 <span className="text-2xl font-bold">修改個人資料</span>
                 <div className="ml-10 mt-10">
-                  <div className={styles.whitetri}>
-                    <div className={styles.graycir}></div>
-
+                  {/* 左上角 卡片與大頭貼 */}
+                  <div className={styles.roundedcard}>
+                    <div className={styles.photostickers}></div>
+                    {/* 右上角 4個長方形提示框 */}
                     <Box display="flex" flexDirection="column" justifyContent="center" pl={60} pt={1} width="200%">
                       <Box alignItems="center" bgcolor="#F0F0F0" borderRadius={10} width="600px">
+                        {/* FIXME:IconButton需要寫功能 */}
                         <IconButton>
                           <Edit />
                         </IconButton>
                         變更頭像
                       </Box>
-                      <Box className={styles.graylongtri}>推薦:正方形.JPG.PNG, 至少1,000像素</Box>
+                      <Box className={styles.promptbox}>推薦:正方形.JPG.PNG, 至少1,000像素</Box>
                       <Box bgcolor="#F0F0F0" borderRadius={10} width="600px">
                         <IconButton>
                           <Edit />
                         </IconButton>
                         變更卡片背景
                       </Box>
-                      <Box className={styles.graylongtri}>推薦:長方形.JPG.PNG, 至少1,000像素</Box>
+                      <Box className={styles.promptbox}>推薦:長方形.JPG.PNG, 至少1,000像素</Box>
                     </Box>
                   </div>
+                  {/*錢包地址(不可更改) FIXME:後續需要錢包地址*/}
                   <div className="mt-12">
                     <Box className={styles.walletaddress}>
                       <TextField
@@ -71,17 +76,31 @@ export default function Editprofile() {
                         defaultValue="0x12345678"
                       />
                     </Box>
-                    <p className="ml-12 text-lg text-gray-300">不可更改</p>
+                    <p className="text-lg text-gray-300">不可更改</p>
                   </div>
                   <p></p>
+                  {/* 下方5個輸入框 */}
                   <div>
+                    {/* 名稱部分 */}
                     <div className={styles.wordsizediv}>
-                      <div className={styles.wordsize}>名稱</div>
-                      <p></p>
+                      <div className="flex items-center justify-between">
+                        <div className={styles.wordsize}>名稱</div>
+                        {/* FIXME:checkname功能需再加  */}
+                        <button className="h-10 w-10 text-lg">checkname</button>
+                      </div>
                       <TextField fullWidth id="outlined-basic" label="請輸入名稱" variant="outlined" />
                       <span className="text-lg text-gray-300">2-20字元</span>
-                      <p></p>
                     </div>
+                    {/* email 部分 */}
+                    <div className={styles.wordsizediv}>
+                      <div className="flex items-center justify-between">
+                        <div className={styles.wordsize}>Email</div>
+                        {/* FIXME:checkemail功能需再加   */}
+                        <button className="h-10 w-10 text-lg">checkemail</button>
+                      </div>
+                      <TextField fullWidth id="outlined-basic" label="請輸入email" variant="outlined" />
+                    </div>
+                    {/* 個人簡介部分 */}
                     <div className={styles.wordsizediv}>
                       <div className={styles.wordsize}>個人簡介</div>
                       <p></p>
@@ -89,6 +108,7 @@ export default function Editprofile() {
                       <span className="text-lg text-gray-300">建議50字以內,最長200字</span>
                       <p></p>
                     </div>
+                    {/* 輸入標籤部分 */}
                     <div className={styles.wordsizediv}>
                       <div className={styles.wordsize}>添加標籤</div>
                       <p></p>
@@ -96,14 +116,17 @@ export default function Editprofile() {
                       <span className="text-lg text-gray-300">建議至多5個主標籤</span>
                       <p></p>
                     </div>
+                    {/* 社群關係連結 */}
                     <div className={styles.wordsizediv}>
                       <div className={styles.wordsize}>社群關係連結</div>
                       <p></p>
                       <TextField fullWidth id="outlined-basic" label="請輸入社群連結" variant="outlined" />
                       <p></p>
                     </div>
+                    {/* 右下角重置與確認按鈕 */}
                     <div className={styles.buttondiv}>
                       <button className={styles.resetbutton}>重置</button>
+                      {/* FIXME:API */}
                       <button className={styles.surebutton}>確認</button>
                     </div>
                   </div>
@@ -113,10 +136,10 @@ export default function Editprofile() {
           </DialogContent>
           <DialogActions>
             <Button autoFocus onClick={handleClose}>
-              Disagree
+              取消
             </Button>
             <Button onClick={handleClose} autoFocus>
-              Agree
+              確認
             </Button>
           </DialogActions>
         </Dialog>

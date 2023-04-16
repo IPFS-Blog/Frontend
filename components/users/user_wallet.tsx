a94334fbd268cff21f3e141de794483f235969c9
inru <sumi143yy@gmail.com>
2023-04-17 00:43:26 +0800

    fix: edit tailwind css

--- components/users/user_wallet.tsx
+++ components/users/user_wallet.tsx
@@ -7,13 +7,11 @@ import { useTheme } from "@mui/material/styles";
 import useMediaQuery from "@mui/material/useMediaQuery";
 import * as React from "react";
 
-import styles from "@/styles/user_wallet.module.css";
-
 export default function ResponsiveDialog() {
   const [open, setOpen] = React.useState(false);
   const theme = useTheme();
-  const fullScreen = useMediaQuery(theme.breakpoints.down("lg"));
-  const [maxWidth] = React.useState<DialogProps["maxWidth"]>("lg");
+  const fullScreen = useMediaQuery(theme.breakpoints.down("md"));
+  const [maxWidth] = React.useState<DialogProps["maxWidth"]>("md");
 
   const handleClickOpen = () => {
     setOpen(true);
@@ -24,7 +22,7 @@ export default function ResponsiveDialog() {
   };
   return (
     <>
-      <div>
+      <div className="">
         {/*  dialog部分皆為彈窗*/}
         <Button variant="outlined" onClick={handleClickOpen}>
           個人錢包
@@ -36,39 +34,47 @@ export default function ResponsiveDialog() {
           onClose={handleClose}
           aria-labelledby="responsive-dialog-title"
         >
-          <DialogTitle id="responsive-dialog-title"> 個人錢包 </DialogTitle>
-          <DialogContent>
-            <div className={styles.all}>
-              <span className="text-3xl font-bold">個人錢包</span>
-              <div className="ml-7  mt-12">
-                <div>
-                  <span className="text-2xl font-bold">資產總覽</span>
-                </div>
-              </div>
-              {/* 交易紀錄按鈕 */}
-              <div className="mr-5 flex justify-end">
-                <button className={styles.transrecordbutton}>交易紀錄</button>
+          <DialogTitle id="responsive-dialog-title" className="bg-gray-200 font-semibold">
+            個人錢包
+          </DialogTitle>
+          <DialogContent className="bg-gray-200 md:w-full lg:w-96">
+            <div>
+              <div className="flex flex-row justify-between">
+                <p className="mx-2 text-xl font-semibold">資產總覽</p>
+                <button className="rounded border border-gray-400 bg-white py-2 px-4 font-semibold text-gray-800 shadow hover:bg-gray-100">
+                  交易紀錄
+                </button>
               </div>
               {/* 不同虛擬幣的圖案以及總額 */}
-              <div className={styles.coincolumn}>
-                <div className={styles.coinimage}></div>
-                <span className={styles.coinword}>10.2210</span>
+              <div className="flex w-full items-center rounded border-b-2 border-l-2 border-gray-600 py-2 text-base font-semibold hover:text-lg">
+                <div className="mx-2 h-9 w-9 border-r-2 bg-gray-300"></div>
+                <div className="flex w-full justify-between">
+                  <p>10.2210</p>
+                  <p>AC</p>
+                </div>
               </div>
-              <div className={styles.coincolumn}>
-                <div className={styles.coinimage}></div>
-                <span className={styles.coinword}>10.0010</span>
-              </div>
-              <div className={styles.buttondiv}>
-                <button className={styles.backbutton}>返回</button>
-                <button className={styles.surebutton}>確認</button>
+              <div className="flex w-full items-center rounded border-b-2 border-l-2 border-gray-600 py-2 text-base font-semibold hover:text-lg">
+                <div className="mx-2 h-9 w-9 border-r-2 bg-gray-300"></div>
+                <div className="flex w-full justify-between">
+                  <p>10.2210</p>
+                  <p>ETH</p>
+                </div>
               </div>
             </div>
           </DialogContent>
-          <DialogActions>
-            <Button autoFocus onClick={handleClose}>
+          <DialogActions className="bg-gray-200">
+            <Button
+              className="rounded border bg-gray-300 py-2 px-4 font-semibold text-gray-800 shadow hover:bg-gray-100"
+              autoFocus
+              onClick={handleClose}
+            >
               返回
             </Button>
-            <Button onClick={handleClose} autoFocus>
+            <Button
+              className="rounded border bg-gray-300 py-2 px-4 font-semibold text-gray-800 shadow hover:bg-gray-100"
+              onClick={handleClose}
+              autoFocus
+            >
               確認
             </Button>
           </DialogActions>

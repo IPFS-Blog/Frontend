9f31d052009827939221b5eb1074ac38d1b2b247
jim0625 <a0931052056@gmail.com>
2023-05-12 00:09:39 +0800

    fix: fixdonatebutton(notall)

--- components/users/UserWallet.tsx
+++ components/users/UserWallet.tsx
@@ -201,7 +201,7 @@ export default function ResponsiveDialog() {
               </div>
               {/* 交換幣值 FIXME:需要個人ETH的金額(超出交換金額)*/}
               {/* FIXME: jim 1. input 固定寬度 */}
-              <div className="flex w-full items-center rounded border-b-2 border-l-2 border-gray-600 py-2 text-base font-semibold  ">
+              <div className="flex w-full items-center rounded border-b-2 border-l-2 border-gray-600 py-2 text-base font-semibold">
                 <div className="flex w-full justify-between">
                   <input
                     type="number"
@@ -226,7 +226,7 @@ export default function ResponsiveDialog() {
                   )}
                 </div>
               </div>
-              <div className="flex w-full items-center rounded border-b-2 border-l-2 border-gray-600 py-2 text-base font-semibold  ">
+              <div className="flex w-full items-center rounded border-b-2 border-l-2 border-gray-600 py-2 text-base font-semibold">
                 <div className="flex w-full justify-between">
                   <input
                     type="number"

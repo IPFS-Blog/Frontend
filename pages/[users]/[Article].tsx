7f49e4ab6968b51991a0ab7fe27b1db22172f3d5
Your Name <Lumi015@example.com>
2023-05-12 06:20:19 +0800

    fix: rightside

--- pages/[users]/[Article].tsx
+++ pages/[users]/[Article].tsx
@@ -3,9 +3,9 @@ import BookmarkAddOutlinedIcon from "@mui/icons-material/BookmarkAddOutlined";
 import FavoriteBorderOutlinedIcon from "@mui/icons-material/FavoriteBorderOutlined";
 import SendIcon from "@mui/icons-material/Send";
 import Avatar from "@mui/material/Avatar";
-import Link from "next/link";
 import { useEffect } from "react";
 import { useDispatch } from "react-redux";
+import Link from "next/link";
 
 import { apiArticleTakeArticle, apiUserGetUserData } from "@/components/api";
 import Comment from "@/components/article/comment/Comment";
@@ -120,17 +120,87 @@ export default function Article({ userData, IsUser, article, createrData }: any)
             追蹤
           </button>
         </div>
+        {/* FIXME:推薦使用者資料 */}
         <div className="my-5 px-2">
-          {/* FIXME: 推薦使用者資料 */}
-          <div className="text-base">推薦使用者</div>
+          <div className="text-base font-semibold">推薦使用者</div>
           <ul className="divide-y divide-blue-200">
-            <li></li>
+            <li className="grid w-full grid-cols-4 py-1">
+              <div className="col-span-3 flex">
+                <Avatar></Avatar>
+                <div className="px-2">
+                  <p>Lin</p>
+                  <p className="line-clamp-2">Hello</p>
+                </div>
+              </div>
+              <button className="my-2 h-8 rounded-full border border-red-500 px-2 font-semibold text-red-500 hover:bg-red-500 hover:text-white">
+                追蹤
+              </button>
+            </li>
+            <li className="grid w-full grid-cols-4 py-1">
+              <div className="col-span-3 flex">
+                <Avatar></Avatar>
+                <div className="px-2">
+                  <p>Rj</p>
+                  <p className="line-clamp-2">
+                    ng duis excepteur esse in duis nostrud occaecat mollit incididunt desaccaecat
+                  </p>
+                </div>
+              </div>
+              <button className="my-2 h-8  rounded-full border border-red-500 px-2 font-semibold text-red-500 hover:bg-red-500 hover:text-white">
+                追蹤
+              </button>
+            </li>
+            <li className="grid w-full grid-cols-4 py-1">
+              <div className="col-span-3 flex">
+                <Avatar></Avatar>
+                <div className="px-2">
+                  <p>Amy</p>
+                  <p className="line-clamp-2">我是一位熱愛設計的設計師</p>
+                </div>
+              </div>
+              <button className="my-2 h-8 rounded-full border border-red-500 px-2 font-semibold text-red-500 hover:bg-red-500 hover:text-white">
+                追蹤
+              </button>
+            </li>
           </ul>
         </div>
+        {/* FIXME:熱門標籤資料 10筆 */}
         <div className="my-5 px-2">
-          <div className="text-base">熱門標籤</div>
-          {/* FIXME: 熱門標籤資料 */}
-          {/* <span className="flex flex-wrap gap-2">{label}</span> */}
+          <div className="text-base font-semibold">熱門標籤</div>
+          <div className="flex flex-wrap gap-2 py-2">
+            <div className="flex items-center rounded-lg border border-gray-500 px-2 text-slate-900 dark:text-white ">
+              <p className="inline-block pr-1 align-middle">#</p> <p className="inline-block align-middle">前端</p>
+            </div>
+            <div className="flex items-center rounded-lg border border-gray-500 px-2 text-slate-900 dark:text-white ">
+              <p className="inline-block pr-1 align-middle">#</p> <p className="inline-block align-middle">狗狗</p>
+            </div>
+            <div className="flex items-center rounded-lg border border-gray-500 px-2 text-slate-900 dark:text-white ">
+              <p className="inline-block pr-1 align-middle">#</p>
+              <p className="inline-block align-middle">網頁設計</p>
+            </div>
+            <div className="flex items-center rounded-lg border border-gray-500 px-2 text-slate-900 dark:text-white ">
+              <p className="inline-block pr-1 align-middle">#</p> <p className="inline-block align-middle">家庭</p>
+            </div>
+            <div className="flex items-center rounded-lg border border-gray-500 px-2 text-slate-900 dark:text-white ">
+              <p className="inline-block pr-1 align-middle">#</p>{" "}
+              <p className="inline-block align-middle">家庭旅遊好去處</p>
+            </div>
+            <div className="flex items-center rounded-lg border border-gray-500 px-2 text-slate-900 dark:text-white ">
+              <p className="inline-block pr-1 align-middle">#</p> <p className="inline-block align-middle">新生季</p>
+            </div>
+            <div className="flex items-center rounded-lg border border-gray-500 px-2 text-slate-900 dark:text-white ">
+              <p className="inline-block pr-1 align-middle">#</p> <p className="inline-block align-middle">Chatgpt</p>
+            </div>
+            <div className="flex items-center rounded-lg border border-gray-500 px-2 text-slate-900 dark:text-white ">
+              <p className="inline-block pr-1 align-middle">#</p> <p className="inline-block align-middle">Java</p>
+            </div>
+            <div className="flex items-center rounded-lg border border-gray-500 px-2 text-slate-900 dark:text-white ">
+              <p className="inline-block pr-1 align-middle">#</p> <p className="inline-block align-middle">C++</p>
+            </div>
+            <div className="flex items-center rounded-lg border border-gray-500 px-2 text-slate-900 dark:text-white ">
+              <p className="inline-block pr-1 align-middle">#</p> <p className="inline-block align-middle">後端</p>
+            </div>
+          </div>
         </div>
       </div>
     </div>

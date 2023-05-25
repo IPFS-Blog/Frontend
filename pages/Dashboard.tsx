494500f7a47e6619a10f86ef428db4dd36b58b1a
jim0625 <a0931052056@gmail.com>
2023-05-26 00:51:43 +0800

    feat: dashboard

--- pages/Dashboard.tsx
+++ pages/Dashboard.tsx
@@ -10,97 +10,97 @@ import * as React from "react";
 
 const StyledTableCell = styled(TableCell)(({ theme }) => ({
   [`&.${tableCellClasses.head}`]: {
-    backgroundColor: theme.palette.common.black,
-    color: theme.palette.common.white,
+    backgroundColor: theme.palette.common.white,
+    color: theme.palette.common.black,
     position: "sticky",
     top: 0,
+    textAlign: "left",
+    fontSize: "2xl",
+    fontWeight: "bold",
   },
   [`&.${tableCellClasses.body}`]: {
-    fontSize: 14,
+    textAlign: "left",
+    maxHeight: "100px",
+    overflow: "hidden",
+    verticalAlign: "top",
+    wordBreak: "break-word",
+    textOverflow: "ellipsis",
   },
 }));
 
-const StyledTableRow = styled(TableRow)(({ theme }) => ({
-  "&:nth-of-type(odd)": {
-    backgroundColor: theme.palette.action.hover,
-  },
+const StyledTableRow = styled(TableRow)(({}) => ({
   // hide last border
   "&:last-child td, &:last-child th": {
     border: 0,
   },
 }));
 
-function createData(title: string, subtitle: string, content: string, time: string, state: boolean) {
-  return { title, subtitle, content, time, state };
+function createData(title: string, subtitle: string, tag: string, content: string, time: string, state: boolean) {
+  return { title, subtitle, tag, content, time, state };
 }
 
 const rows = [
-  createData("標題1", "副標題1", "內文1", "2023/5/21", true),
-  // ... (略去其余数据)
-  createData("標題6", "副標題6", "內文6", "2023/5/21", true),
-  createData("標題6", "副標題6", "內文6", "2023/5/21", true),
-  createData("標題6", "副標題6", "內文6", "2023/5/21", true),
-  createData("標題6", "副標題6", "內文6", "2023/5/21", true),
-  createData("標題6", "副標題6", "內文6", "2023/5/21", true),
-  createData("標題6", "副標題6", "內文6", "2023/5/21", true),
-  createData("標題6", "副標題6", "內文6", "2023/5/21", true),
-  createData("標題6", "副標題6", "內文6", "2023/5/21", true),
-  createData("標題6", "副標題6", "內文6", "2023/5/21", true),
-  createData("標題6", "副標題6", "內文6", "2023/5/21", true),
-  createData("標題6", "副標題6", "內文6", "2023/5/21", true),
-  createData("標題6", "副標題6", "內文6", "2023/5/21", true),
-  createData("標題6", "副標題6", "內文6", "2023/5/21", true),
-  createData("標題6", "副標題6", "內文6", "2023/5/21", true),
-  createData("標題6", "副標題6", "內文6", "2023/5/21", true),
-  createData("標題6", "副標題6", "內文6", "2023/5/21", true),
-  createData("標題6", "副標題6", "內文6", "2023/5/21", true),
-  createData("標題6", "副標題6", "內文6", "2023/5/21", true),
-  createData("標題6", "副標題6", "內文6", "2023/5/21", true),
-  createData("標題6", "副標題6", "內文6", "2023/5/21", true),
-  createData("標題6", "副標題6", "內文6", "2023/5/21", true),
-  createData("標題6", "副標題6", "內文6", "2023/5/21", true),
-  createData("標題6", "副標題6", "內文6", "2023/5/21", true),
-  createData("標題6", "副標題6", "內文6", "2023/5/21", true),
-  createData("標題6", "副標題6", "內文6", "2023/5/21", true),
-  createData("標題6", "副標題6", "內文6", "2023/5/21", true),
-  createData("標題6", "副標題6", "內文6", "2023/5/21", true),
-  createData("標題6", "副標題6", "內文6", "2023/5/21", true),
-  createData("標題6", "副標題6", "內文6", "2023/5/21", true),
-  createData("標題6", "副標題6", "內文6", "2023/5/21", true),
-  createData("標題6", "副標題6", "內文6", "2023/5/21", true),
-  createData("標題6", "副標題6", "內文6", "2023/5/21", true),
-  createData("標題6", "副標題6", "內文6", "2023/5/21", true),
-  createData("標題6", "副標題6", "內文6", "2023/5/21", true),
-  createData("標題6", "副標題6", "內文6", "2023/5/21", true),
+  createData(
+    "標題123456標題1標標題123456標題1標題1標題1標題1標題1標題1標題1標題1標題1標題1標題1標題1標題1標題1標題1標題1標題1標題1標題1標題1標題1標題1標題1標題1標題1標題1標題1標題1標題1標題1標題1標題1標題1標題1標題1標題1標題1標題1標題1標題1標題1標題1標題1標題1標題1標題1標題1標題1標題1標題1標題1標題1標題1題1標題1標題1標題1標題1標題1標題1標題1標題1標題1標題1標題1標題1標題1標題1標題1標題1標題1標題1標題1標題1標題1標題1標題1標題1標題1標題1標題1標題1標題1標題1標題1標題1標題1標題1標題1標題1標題1標題1標題1標題1標題1標題1標題1標題1標題1標題1標題1標題1標題1標題1標題1",
+    "副標題1",
+    "tag",
+    "內文1",
+    "2023/5/21",
+    true,
+  ),
 ];
 export default function CustomizedTables() {
   return (
-    <TableContainer component={Paper} sx={{ maxHeight: "200vh", overflow: "auto" }}>
+    <TableContainer component={Paper}>
       <Table stickyHeader aria-label="sticky table">
-        <TableHead sx={{ maxHeight: "900px" }}>
+        <TableHead>
           <TableRow>
-            <StyledTableCell align="center" colSpan={5}>
+            <StyledTableCell align="center" colSpan={6} className="text-2xl">
               文章
             </StyledTableCell>
           </TableRow>
-          <TableRow>
-            <StyledTableCell align="right">標題</StyledTableCell>
-            <StyledTableCell align="right">副標</StyledTableCell>
-            <StyledTableCell align="right">內文</StyledTableCell>
-            <StyledTableCell align="right">時間</StyledTableCell>
-            <StyledTableCell align="right">狀態</StyledTableCell>
+          <TableRow className="rounded-lg">
+            <StyledTableCell className="text-base" sx={{ width: "20%" }}>
+              標題
+            </StyledTableCell>
+            <StyledTableCell className="text-base" sx={{ width: "20%" }}>
+              副標
+            </StyledTableCell>
+            <StyledTableCell className="text-base" sx={{ width: "10%" }}>
+              tag
+            </StyledTableCell>
+            <StyledTableCell className="text-base" sx={{ width: "30%" }}>
+              內文
+            </StyledTableCell>
+            <StyledTableCell className="text-base" sx={{ width: "10%" }}>
+              時間
+            </StyledTableCell>
+            <StyledTableCell className="text-base" sx={{ width: "10%" }}>
+              狀態
+            </StyledTableCell>
           </TableRow>
         </TableHead>
         <TableBody>
           {rows.map(row => (
             <StyledTableRow key={row.title}>
               <StyledTableCell component="th" scope="row">
-                {row.title}
+                <div className="overflow-hidden text-base">{row.title}</div>
               </StyledTableCell>
-              <StyledTableCell align="right">{row.subtitle}</StyledTableCell>
-              <StyledTableCell align="right">{row.content}</StyledTableCell>
-              <StyledTableCell align="right">{row.time}</StyledTableCell>
-              <StyledTableCell align="right">{row.state}</StyledTableCell>
+              <StyledTableCell align="left">
+                <div className="overflow-hidden text-base">{row.subtitle}</div>
+              </StyledTableCell>
+              <StyledTableCell align="left">
+                <div className="overflow-hidden text-base">{row.tag}</div>
+              </StyledTableCell>
+              <StyledTableCell align="left">
+                <div className="overflow-hidden text-base">{row.content}</div>
+              </StyledTableCell>
+              <StyledTableCell align="left">
+                <div className="overflow-hidden text-base">{row.time}</div>
+              </StyledTableCell>
+              <StyledTableCell align="left">
+                <div className="overflow-hidden text-base">{row.state.toString()}</div>
+              </StyledTableCell>
             </StyledTableRow>
           ))}
         </TableBody>

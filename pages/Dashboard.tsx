fdca8e309bdbaa06f3d3742e1cfb8a1010bb8ce5
jim0625 <a0931052056@gmail.com>
2023-06-08 19:28:45 +0800

    fix: add_edit&remove

--- pages/Dashboard.tsx
+++ pages/Dashboard.tsx
@@ -29,39 +29,39 @@ const StyledTableRow = styled(TableRow)(({}) => ({
   },
 }));
 /*  FIXME: Andy 拿取文章資訊*/
-function createData(title: string, subtitle: string, tag: string, content: string, time: string, state: boolean) {
-  return { title, subtitle, tag, content, time, state };
+function createData(state: boolean, title: string, subtitle: string, tag: string, content: string, time: string) {
+  return { state, title, subtitle, tag, content, time };
 }
 
 const rows = [
   createData(
+    true,
     "標題123456標題1標標題123456標題1標題1標題1標題1標題1標題1標題1標題1標題1標題1標題1標題1標題1標題1標題1標題1標題1標題1標題1標題1標題1標題1標題1標題1標題1標題1標題1標題1標題1標題1標題1標題1標題1標題1標題1標題1標題1標題1標題1標題1標題1標題1標題1標題1標題1標題1標題1標題1標題1標題1標題1標題1標題1題1標題1標題1標題1標題1標題1標題1標題1標題1標題1標題1標題1標題1標題1標題1標題1標題1標題1標題1標題1標題1標題1標題1標題1標題1標題1標題1標題1標題1標題1標題1標題1標題1標題1標題1標題1標題1標題1標題1標題1標題1標題1標題1標題1標題1標題1標題1標題1標題1標題1標題1標題1",
     "副標題1",
     "tag",
     "內文1內文1內文1內文1內文1內文1內文1內文1內文1內文1內文1內文1內文1內文1內文1內文1內文1內文1內文1內文1內文1內文1內文1內文1內文1內文1內文1內文1內文1內文1內文1內文1內文1內文1內文1內文1內文1內文1內文1內文1內文1內文1內文1內文1內文1內文1內文1內文1內文1內文1內文1內文1內文1內文1內文1內文1內文1內文1內文1內文1內文1內文1內文1內文1內文1內文1內文1內文1內文1內文1內文1內文1內文1內文1內文1內文1內文1內文1內文1內文1內文1內文1內文1內文1內文1內文1內文1內文1內文1內文1內文1",
     "2023/5/21",
-    true,
   ),
   createData(
+    false,
     "標題123456標題1標標題123456標題1標題1標題1標題1標題1標題1標題1標題1標題1標題1標題1標題1標題1標題1標題1標題1標題1標題1標題1標題1標題1標題1標題1標題1標題1標題1標題1標題1標題1標題1標題1標題1標題1標題1標題1標題1標題1標題1標題1標題1標題1標題1標題1標題1標題1標題1標題1標題1標題1標題1標題1標題1標題1題1標題1標題1標題1標題1標題1標題1標題1標題1標題1標題1標題1標題1標題1標題1標題1標題1標題1標題1標題1標題1標題1標題1標題1標題1標題1標題1標題1標題1標題1標題1標題1標題1標題1標題1標題1標題1標題1標題1標題1標題1標題1標題1標題1標題1標題1標題1標題1標題1標題1標題1標題1",
     "副標題1",
     "tag",
     "內文1內文1內文1內文1內文1內文1內文1內文1內文1內文1內文1內文1內文1內文1內文1內文1內文1內文1內文1內文1內文1內文1內文1內文1內文1內文1內文1內文1內文1內文1內文1內文1內文1內文1內文1內文1內文1內文1內文1內文1內文1內文1內文1內文1內文1內文1內文1內文1內文1內文1內文1內文1內文1內文1內文1內文1內文1內文1內文1內文1內文1內文1內文1內文1內文1內文1內文1內文1內文1內文1內文1內文1內文1內文1內文1內文1內文1內文1內文1內文1內文1內文1內文1內文1內文1內文1內文1內文1內文1內文1內文1",
     "2023/5/21",
-    false,
   ),
 ];
 //文章狀態button
 function renderButton(isTrue: any) {
   if (isTrue) {
     return (
-      <button className=" mx-5 rounded-full border  bg-blue-500 py-2 px-10 font-semibold  text-white tablet:mx-2 tablet:px-5">
+      <button className=" mx-5 w-4/5 rounded-full border  bg-gray-400 py-2 px-10 font-semibold  text-white tablet:mx-2 tablet:px-5">
         已發布
       </button>
     );
   } else {
     return (
-      <button className="mx-5 rounded-full border bg-yellow-400 py-2 px-10 font-semibold text-white tablet:mx-2 tablet:px-5">
+      <button className="mx-5 w-4/5 rounded-full border bg-yellow-400 py-2 px-10 font-semibold text-white tablet:mx-2 tablet:px-5">
         草稿
       </button>
     );
@@ -74,34 +74,36 @@ export default function CustomizedTables() {
       <Table stickyHeader aria-label="sticky table">
         <TableHead>
           <TableRow>
-            <StyledTableCell align="center" colSpan={6} className="bg-purple-100 text-2xl">
+            <StyledTableCell align="center" colSpan={7} className="bg-purple-100 text-2xl">
               文章
             </StyledTableCell>
           </TableRow>
           <TableRow className="rounded-lg ">
-            <StyledTableCell className="bg-blue-300 text-base" sx={{ width: "20%" }}>
-              標題
-            </StyledTableCell>
-            <StyledTableCell className="bg-blue-300 text-base" sx={{ width: "20%" }}>
-              副標
-            </StyledTableCell>
-            <StyledTableCell className="bg-blue-300 text-base" sx={{ width: "10%" }}>
-              tag
-            </StyledTableCell>
-            <StyledTableCell className="bg-blue-300 text-base" sx={{ width: "30%" }}>
-              內文
-            </StyledTableCell>
-            <StyledTableCell className="bg-blue-300 text-base" sx={{ width: "10%" }}>
-              時間
-            </StyledTableCell>
-            <StyledTableCell className="bg-blue-300 text-base" sx={{ width: "10%" }}>
+            <StyledTableCell className="bg-blue-300 text-base" sx={{ width: "8%" }}>
               狀態
             </StyledTableCell>
+            <StyledTableCell className="bg-blue-300 text-base" sx={{ width: "15%" }}>
+              標題
+            </StyledTableCell>
+            <StyledTableCell className="bg-blue-300 text-base" sx={{ width: "15%" }}>
+              副標
+            </StyledTableCell>
+            <StyledTableCell className="bg-blue-300 text-base" sx={{ width: "11%" }}>
+              tag
+            </StyledTableCell>
+            <StyledTableCell className="bg-blue-300 text-base" sx={{ width: "25%" }}>
+              內文
+            </StyledTableCell>
+            <StyledTableCell className="bg-blue-300 text-base" sx={{ width: "16%" }}>
+              時間
+            </StyledTableCell>
+            <StyledTableCell className="bg-blue-300 text-base" sx={{ width: "10%" }}></StyledTableCell>
           </TableRow>
         </TableHead>
         <TableBody>
           {rows.map(row => (
             <StyledTableRow key={row.title}>
+              <StyledTableCell align="left">{renderButton(row.state)}</StyledTableCell>
               <StyledTableCell component="th" scope="row">
                 <div className="h-24 overflow-hidden text-base">{row.title}</div>
               </StyledTableCell>
@@ -117,7 +119,14 @@ export default function CustomizedTables() {
               <StyledTableCell align="left">
                 <div className="overflow-hidden text-base">{row.time}</div>
               </StyledTableCell>
-              <StyledTableCell align="left">{renderButton(row.state)}</StyledTableCell>
+              <StyledTableCell align="left">
+                <button className=" mx-5 w-4/5 rounded-full border  bg-red-500 py-2 px-10 font-semibold  text-white tablet:mx-2 tablet:px-5">
+                  刪除
+                </button>
+                <button className=" mx-5 w-4/5 rounded-full border  bg-blue-500 py-2 px-10 font-semibold  text-white tablet:mx-2 tablet:px-5">
+                  編輯
+                </button>
+              </StyledTableCell>
             </StyledTableRow>
           ))}
         </TableBody>

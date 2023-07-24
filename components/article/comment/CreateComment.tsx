51edbb5bcd48e1cc7055a5f90b134602402d1e20
Your Name <Lumi015@example.com>
2023-07-24 11:47:36 +0800

    fix: Comment-Article

---
+++ components/article/comment/CreateComment.tsx
@@ -0,0 +1,63 @@
+import SendIcon from "@mui/icons-material/Send";
+import Avatar from "@mui/material/Avatar";
+import { useState } from "react";
+
+import { _apiCheckJwt, apiArticleCommentCreate, apiArticleTakeAllArticle } from "@/components/api";
+
+const CreateComment = (props: any) => {
+  // TODO: Handle funtion
+  const [articleid] = useState(props.articleid);
+  const [Comment, setComment] = useState(""); // 留言
+
+  async function Create() {
+    let jwt = "";
+    await _apiCheckJwt().then((res: any) => (jwt = res.data.jwt));
+    const articleId = Number(articleid);
+    //console.log(typeof articleId);
+    //const data = String({ Comment });
+    console.log(Comment);
+    apiArticleCommentCreate(jwt, articleId, Comment)
+      .then(async (res: any) => {
+        console.log("成功", res);
+        await apiArticleTakeAllArticle("?aid=" + props.articleid)
+          .then(async res => {
+            const { comments } = res.data.article;
+            console.log("!!!", comments);
+            props.setComments(comments);
+          })
+          .catch(() => {
+            return {
+              notFound: true,
+            };
+          });
+      })
+      .catch((error: any) => {
+        console.log("錯誤:", error);
+      });
+    //axios.post("http://192.168.0.16:3000/api/v1/articles/1/comment", { comment: Comment });
+  }
+
+  return (
+    <div>
+      <div className="flex items-center bg-gray-50 px-3 py-1 dark:bg-gray-700">
+        <Avatar className="h-auto w-10 rounded-full" src={props.picture} alt="not find Avatar" />
+        <p className="mx-2">{props.username}</p>
+        <textarea
+          id="chat"
+          className="mx-4 block w-full rounded-lg border border-gray-300 bg-white p-2.5 text-sm text-gray-900 focus:border-blue-500 focus:ring-blue-500 dark:border-gray-600 dark:bg-gray-800 dark:text-white dark:placeholder:text-gray-400 dark:focus:border-blue-500 dark:focus:ring-blue-500"
+          placeholder="輸入留言..."
+          onChange={e => setComment(e.target.value)}
+        ></textarea>
+        <button
+          //type="submit"
+          className="inline-flex cursor-pointer justify-center rounded-full p-2 text-blue-600 hover:bg-gray-300 dark:text-blue-500 dark:hover:bg-gray-100"
+          onClick={Create}
+        >
+          <SendIcon />
+        </button>
+      </div>
+    </div>
+  );
+};
+
+export default CreateComment;

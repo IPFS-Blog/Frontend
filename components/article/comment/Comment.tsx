a34f97c7a2b30380e1712f82861c086662ce497e
Your Name <Lumi015@example.com>
2023-07-24 11:39:50 +0800

    fix: comment style

--- components/article/comment/Comment.tsx
+++ components/article/comment/Comment.tsx
@@ -12,20 +12,17 @@ const Comment = (props: any) => {
         <Avatar className="h-auto w-10 rounded-full" src="" alt="not find Avatar" />
         {/* <Avatar className="h-auto w-10 rounded-full" src={`${User.profile.picture}`} alt="" /> */}
         <p className="mx-2">{username}</p>
-        <div className="flex w-full flex-col">
-          <span className="mx-4 block w-full rounded-lg border border-gray-300 bg-gray-200 p-2.5 text-sm text-gray-900 focus:border-blue-500 focus:ring-blue-500 dark:border-gray-600 dark:bg-gray-800 dark:text-white dark:placeholder:text-gray-400 dark:focus:border-blue-500 dark:focus:ring-blue-500">
+        <div className="flex w-full flex-col mx-4">
+          <span className="block w-full rounded-lg border border-gray-300 bg-gray-200 p-2.5 text-sm text-gray-900 focus:border-blue-500 focus:ring-blue-500 dark:border-gray-600 dark:bg-gray-800 dark:text-white dark:placeholder:text-gray-400 dark:focus:border-blue-500 dark:focus:ring-blue-500">
             {props.contents}
           </span>
-          <br />
           <div>
-            <p>
-              時間:{props.updateAt.substr(0, 10)} 讚數:{props.like}
-            </p>
+            時間:{props.updateAt.substr(0, 10)} 讚數:{props.like}
           </div>
         </div>
         <button
           type="submit"
-          className="inline-flex justify-center rounded-full p-2 text-blue-600 hover:bg-gray-300 dark:text-blue-500 dark:hover:bg-gray-100"
+          className="inline-flex cursor-pointer justify-center rounded-full p-2 text-blue-600 hover:bg-gray-300 dark:text-blue-500 dark:hover:bg-gray-100"
         >
           <ThumbUpOutlinedIcon />
           <span className="sr-only">按讚</span>

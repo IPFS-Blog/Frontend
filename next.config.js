a5d2215daf239f95fc19fd6ab0eb2c15f53ed5f0
a20688392 <a20688392@gmail.com>
2023-08-30 22:26:59 +0800

    ci: reset Sentry settings and adjust packages

--- next.config.js
+++ next.config.js
@@ -4,12 +4,4 @@ const nextConfig = {
   reactStrictMode: true,
 };
 
-module.exports = {
-  webpack: (config, options) => {
-    if (!options.isServer) {
-      config.resolve.alias["@sentry/node"] = "@sentry/browser";
-    }
-    return config;
-  },
-  ...nextConfig,
-};
+module.exports = nextConfig;

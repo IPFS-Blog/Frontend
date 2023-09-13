86f1b28513e0ff625835504661622006ea312931
uinnnnnn <sumi143yy@gmail.com>
2023-09-13 11:46:53 +0800

    refactor: link to correct page

--- pages/docs/index.tsx
+++ pages/docs/index.tsx
@@ -4,7 +4,7 @@ export default function NetworkInstructions() {
   return (
     <div>
       <div className="m-2 text-5xl text-blue-900 dark:text-blue-600">文件</div>
-      <Link href="./NetworkInstructions" className="px-2 underline ">
+      <Link href="/docs/NetworkInstructions" className="px-2 underline ">
         如何加入我們的區塊鏈網路
       </Link>
     </div>

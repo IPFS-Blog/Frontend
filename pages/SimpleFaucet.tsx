7281422bc6729c0b20705901a8e3c6ccfbf0c6c2
林映如 <sumi143yy@gmail.com>
2023-09-23 02:42:55 +0800

    feat: is in our blockchain or not, add doc "how to Join Ac Coin" JoinCoin.tsx

--- pages/SimpleFaucet.tsx
+++ pages/SimpleFaucet.tsx
@@ -1,18 +1,22 @@
 import { AlertProps, Snackbar } from "@mui/material";
 import MuiAlert from "@mui/material/Alert";
+import { useRouter } from "next/router";
 import { useEffect, useState } from "react";
 import * as React from "react";
 import Web3 from "web3";
 
+import AlertDialogSlide from "@/components/alert/AlertDialogSlide";
 import { FaucetFunction } from "@/helpers/Contract/FaucetFunction";
+import { CheckChainIdFunction } from "@/helpers/users/CheckChainIdFunction";
 
 import Mining from "./loading/mining";
 
 export default function SimpleFaucet() {
-  // TODO: Handle funtion
+  // TODO: Handle function
   const [address, setAddress] = useState("");
   const [ETH, setETH] = useState("");
-
+  const [isInChainId, setIsInChainId] = useState(false);
+  const router = useRouter();
   useEffect(() => {
     const connect = async () => {
       // TODO: 拿取帳號
@@ -25,6 +29,13 @@ export default function SimpleFaucet() {
         // TODO: 拿取ETH
         const ethBalance = web3.utils.fromWei(await web3.eth.getBalance(accounts[0]));
         setETH(ethBalance);
+      }
+      const InChainId = await CheckChainIdFunction();
+      if (InChainId == false) {
+        setAlertDialogSlide(true);
+        setIsInChainId(true);
+      } else if (InChainId == "Fix") {
+        window.alert("區塊鏈維修中");
       }
     };
     connect();
@@ -64,12 +75,13 @@ export default function SimpleFaucet() {
         });
     }
   };
-  // TODO: UI funtion
+  // TODO: UI function
   const [isLoading, setIsLoading] = useState(false);
   const [alertJoinCoinFail, setalertJoinCoinFail] = useState(false);
   const [alertTakeMoneyFail, setalertTakeMoneyFail] = useState(false);
   const [alertJoinCoinSucess, setalertJoinCoinSucess] = useState(false);
   const [alertTakeMoneySucess, setalertTakeMoneySucess] = useState(false);
+  const [alertDialogSlide, setAlertDialogSlide] = useState(false);
   // TODO: 領錢
   const [transfermoney] = useState(true);
 
@@ -83,6 +95,9 @@ export default function SimpleFaucet() {
     setalertJoinCoinFail(false);
   };
 
+  function jumpPage() {
+    router.push("./docs/NetworkInstructions");
+  }
   //material ui toast
   const Alert = React.forwardRef<HTMLDivElement, AlertProps>(function Alert(props, ref) {
     return <MuiAlert elevation={6} ref={ref} variant="filled" {...props} />;
@@ -95,31 +110,43 @@ export default function SimpleFaucet() {
         獲取 <span className="font-semibold">ETH</span> 代幣
       </div>
       <div className="my-5 mx-2 text-base ">
-        <div className="my-1">錢包地址</div>
-
-        <div className="my-1 w-24 min-w-fit rounded-md border border-slate-200 bg-gray-200 py-3 px-2 text-slate-600 hover:border-2 hover:border-solid hover:border-blue-300 hover:ring">
+        <div className="my-2">錢包地址</div>
+        <div className="my-2 w-24 min-w-fit rounded-md border border-slate-200 bg-gray-200 py-3 px-2 text-slate-600 hover:border-2 hover:border-solid hover:border-blue-300 hover:ring">
           {address}
         </div>
-        <br></br>
-        <div className="my-1">目前擁有</div>
-
-        <div className="my-1 w-24 min-w-fit rounded-md border border-slate-200 bg-gray-200 py-3 px-2 text-slate-600 hover:border-2 hover:border-solid hover:border-blue-300 hover:ring">
+        <div className="my-2">目前擁有</div>
+        <div className="my-2 w-24 min-w-fit rounded-md border border-slate-200 bg-gray-200 py-3 px-2 text-slate-600 hover:border-2 hover:border-solid hover:border-blue-300 hover:ring">
           {ETH}
         </div>
-        <br></br>
         {/* TODO: 領錢 */}
         {isLoading ? (
           <Mining />
+        ) : isInChainId ? (
+          <div className="my-2 w-fit rounded-full border  bg-red-500 py-2 px-10 font-semibold  text-white tablet:mx-2 tablet:px-5">
+            未加入 IPFS 幣記 網路，請先加入才即可獲取ETH代幣
+          </div>
         ) : (
-          <button
-            className="rounded-lg bg-blue-900 px-6 py-2 text-sm text-blue-50 shadow-blue-400/30 transition-colors duration-300 hover:bg-blue-500"
-            onClick={takemoney}
-          >
-            領取10個ETHER
-          </button>
+          <div className="flex flex-row">
+            <div className="my-2 w-fit rounded border  bg-green-500 py-2 px-10 font-semibold  text-white tablet:px-5">
+              已加入 IPFS 幣記 網路
+            </div>
+            <button
+              className="my-2 w-fit rounded bg-blue-900 px-6 py-2 text-sm text-blue-50 shadow-blue-400/30 transition-colors duration-300 hover:bg-blue-500"
+              onClick={takemoney}
+            >
+              領取10個ETHER
+            </button>
+          </div>
         )}
         {transfermoney ? null : <h1>轉帳成功</h1>}
       </div>
+      {alertDialogSlide && (
+        <AlertDialogSlide
+          handlefunction={jumpPage}
+          title={<p>未加入 IPFS 幣記 網路</p>}
+          message={<div>點擊 同意 將可以觀看我們提供的文件 &Prime;如何加入我們的Network&Prime;</div>}
+        />
+      )}
       <Snackbar open={alertTakeMoneySucess} autoHideDuration={6000} onClose={alertHandleClose}>
         <Alert onClose={alertHandleClose} severity="success" sx={{ width: "100%" }}>
           領錢成功

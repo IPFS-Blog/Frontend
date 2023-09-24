9b41e9272bf215f48ac2af05633b0b53d27f38b7
林映如 <113336518+uinnnnnn@users.noreply.github.com>
2023-09-24 18:09:39 +0800

    Fix/correct network instructions router, Is in our blockchain or not (#63)
    * fix: correct NetworkInstructions Router
    
    * fix: /docs has no navbar, correct word
    
    * refactor: link to correct page
    
    * fix: /docs have sidebar
    
    * feat: is in our blockchain or not, add doc "how to Join Ac Coin" JoinCoin.tsx

--- pages/docs/index.tsx
+++ pages/docs/index.tsx
@@ -1,12 +1,21 @@
 import Link from "next/link";
 
-export default function NetworkInstructions() {
+export default function Index() {
   return (
     <div>
       <div className="m-2 text-5xl text-blue-900 dark:text-blue-600">文件</div>
-      <Link href="./NetworkInstructions" className="px-2 underline ">
-        如何加入我們的區塊鏈網路
-      </Link>
+      <ul>
+        <li>
+          <Link href="/docs/NetworkInstructions" className="px-2 underline ">
+            如何加入我們的區塊鏈網路
+          </Link>
+        </li>
+        <li>
+          <Link href="/docs/JoinCoin" className="px-2 underline ">
+            如何加入我們的 AC 代幣
+          </Link>
+        </li>
+      </ul>
     </div>
   );
 }
